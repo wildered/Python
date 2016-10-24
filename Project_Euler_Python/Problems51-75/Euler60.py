c8d17bfa02027a25b73891812d72b57194c04e5c
= <guillermo_wildschut@hotmail.com>
2016-10-24 22:27:33 +0200

    new solution for 60

---
+++ Project_Euler_Python/Problems51-75/Euler60.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 21:42:50 2016
+
+@author: Home
+"""
+
+from eulerTools import isPrime
+from eulerTools import sieve_for_primes_to
+
+from datetime import datetime
+startTime = datetime.now()
+hoog = 10000
+
+
+def digitsum(n):
+    return sum([int(c) for c in str(n)])
+
+primes = sieve_for_primes_to(hoog)
+primes.remove(2)
+arr = [0 for i in xrange(hoog)]
+d = {}
+for i in range(len(primes)):
+    d[primes[i]] = i
+
+def func():
+    idx = 0
+    m = len(primes)
+    
+    while True:
+        p = primes[idx]
+        cdx = idx+1    
+        
+        while cdx < m:
+            temp = [p]
+            a1 = digitsum(p)%3
+            
+            bdx = cdx
+            while bdx < m:
+                new_p = primes[bdx]
+                if ((digitsum(new_p))%3) != a1:
+                    bdx += 1
+                    continue
+                flag = True
+                for pr in temp:
+                    front = int(str(pr) + str(new_p))
+                    back = int(str(new_p)+ str(pr))
+                    if not(isPrime(front) and isPrime(back)):
+                        flag = False
+                        break
+                if flag:
+                    temp.append(new_p)
+                if len(temp) >= 5:
+                    print "FOUND ONE"
+                    print temp
+                    print "Result = " + str(sum(temp))
+                    return
+                    
+                bdx += 1
+            
+            if len(temp) > 1:
+                cdx = d[temp[1]]+1
+            else:
+                break
+        
+        idx += 1
+
+func()
+
+print "SOLUTION FOUND: "
+print datetime.now() - startTime
+start = 3
+#time 6s
+    
+
+
+

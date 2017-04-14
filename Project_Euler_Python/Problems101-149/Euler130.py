1e2bb58198ed692dd796b46014369430e39768dc
= <guillermo_wildschut@hotmail.com>
2017-04-14 23:36:44 +0200

    New Solution

--- Project_Euler_Python/Problems101-149/Euler130.py
+++ Project_Euler_Python/Problems101-149/Euler130.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 14 00:24:10 2017
+Created on Fri Apr 14 16:08:02 2017
 
 @author: Home
 """
 
-#See math appendix
+#See problem 129
 
 from math import gcd
-from eulerTools import primeFactor, eulerPhi
+from eulerTools import primeFactor, eulerPhi, sieve_for_primes_to
 from datetime import datetime
+
+primes = set(sieve_for_primes_to(int(1e6)))
 
 idx = 0
 def order_of_10(n):
@@ -25,15 +27,18 @@ def order_of_10(n):
 
 
 startTime = datetime.now()
-n = int(1e6)
-while True:
-    if gcd(n, 10) != 1:
+n = 90
+idx = 0
+resSum = 0
+while idx < 25:
+    if gcd(n, 10) != 1 or n in primes:
         n += 1
         continue
-    if order_of_10(9*n) > 1e6:
-        break
+    if (n-1)%order_of_10(9*n) == 0:
+        resSum += n
+        idx += 1
     n += 1
 
-print("Result =",  n)
+print("Result =",  resSum)
 print(datetime.now() - startTime)
 

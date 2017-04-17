026eee86ec99c3c30b4e462132b0292bc474c88f
= <guillermo_wildschut@hotmail.com>
2017-04-17 18:28:20 +0200

    Improved solution

--- Project_Euler_Python/Problems101-149/Euler120.py
+++ Project_Euler_Python/Problems101-149/Euler120.py
@@ -8,6 +8,8 @@ Created on Mon Apr 17 18:00:24 2017
 #use the binomial theorem + modular arithmetic  => answer
 #n even can be skipped, n odd gives unique smallest n for maximal answer.
 #time: 0.5ms
+#Bonus: Sum can also be written as sum([2*k*(4*k+3) for k in range(1, 500)])
+#which takes 9µs. Direct formula for the sum can also be found. 
 
 from datetime import datetime
 from timeit import default_timer as timer
